a4a745eca90861011b1805d8e0a8c684845431a1
Q Z <13253467705@163.com>
2023-10-19 10:17:28 +0800

    1、功能优化

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-/** 安装三方依赖库
- * （1）swift package update
- * （2）swift package generate-xcodeproj
- */
 
 import PackageDescription
 
@@ -11,25 +7,19 @@ let package = Package(
     name: "NNKitEx",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NNKitEx",
             targets: ["NNKitEx"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.17.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.2.0")),
         .package(url: "https://github.com/Zean-Technology-Co-Ltd/FoundationEx.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "NNKitEx",
             dependencies: [
               "SDWebImage",
-              "RxSwift",
-              .product(name: "RxCocoa", package: "RxSwift"),
               "FoundationEx"
             ]),
 //        .target(
@@ -41,8 +31,7 @@ let package = Package(
         .testTarget(
             name: "NNKitExTests",
             dependencies: [
-                "NNKitEx",
-                .product(name: "RxTest", package: "RxSwift"),
+                "NNKitEx"
             ]),
     ]
 )
